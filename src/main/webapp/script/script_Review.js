970c60a58b3f0226b0bbff3da0eb960855ba0da6
suduper <87802345+suduper@users.noreply.github.com>
2022-01-03 16:25:52 +0900

    Add files via upload

--- src/main/webapp/script/script_Review.js
+++ src/main/webapp/script/script_Review.js
@@ -1,8 +1,14 @@
 
 
-function read(param) {
-	location.href="Read.jsp?num="+param;
-}	
+function ReviewRead(param) {
+	location.href="ReviewRead.jsp?num="+param;
+}
+
+function movePage(p1){
+	let param = "ReviewList.jsp?nowPage="+p1;
+	location.href = param;
+}
+
 
 
 
@@ -55,6 +61,69 @@ $(function(){
 		
 	});
 // 게시판 등록 끝
+
+	$("#modBtn").click(function(){
+		
+		let nowPage = $("input#nowPage").val().trim();
+		let num = $("input#num").val().trim();
+		
+		let url = "../Review/ReviewUpdate.jsp?num="+num+"&nowPage="+nowPage;
+		location.href=url;
+		
+	})
+	
+		$("#regBtnMod").click(function(){
+		let uName = $("#uName").val().trim();
+		let subject = $("#subject").val().trim();
+		let pass = $("#pass").val().trim();
+		
+		if(uName == ""){
+			alert("이름을 입력하세요");
+			$("#uName").focus;
+		} else if(subject == ""){
+			alert("제목을 입력하세요");
+			$("#subject").focus;
+		}else if(pass == ""){
+			alert("비밀번호를 입력하세요");
+			$("#pass").focus;
+		}else{
+			$("#UpdateFrm").submit();
+		}
+		
+	});
+	
+		$("#delBtn").click(function(){
+		
+		let nowPage = $("input#nowPage").val().trim();
+		let num = $("input#num").val().trim();
+		
+		let url = "../Review/ReviewDelete.jsp?num="+num+"&nowPage="+nowPage;
+		location.href=url;
+		
+	});
+	
+	   $("#delSbmBtn").click(function(){
+		
+		let pass = $("input#pass").val().trim();
+		
+		if(pass == ""){
+			$("input#pass").focus();
+			alert("비밀번호를 입력하세요");
+			return;
+		} else {
+			
+			let delChk = confirm("게시물을 삭제합니다");
+			if(delChk){
+				$("#delFrm").submit();
+			} else{
+				alert("삭제를 취소했습니다");
+				return;
+			}
+		}
+		
+		
+	});
+
 
 
 });
