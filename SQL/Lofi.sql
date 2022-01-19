590e790baf061eb65392ecf1a71ab4a4f172092b
InGukOh <dhdlsrnr1231@naver.com>
2022-01-19 17:07:27 +0900

    SQL_Update

--- SQL/Lofi.sql
+++ SQL/Lofi.sql
@@ -126,6 +126,7 @@ select * from userOrder where uID ='user1231' and addDate = '2022-01-14 18:31:03
 delete from userBasket where uID = 'user1231' and addDate = '2022-01-17 18:15:39' and goodsName ='여름옷 시리즈~_2201140900' and Scount=0  and Mcount= 0 and Lcount= 0 and XLcount= 1 and allcount= 1 and calcRes= 80000 and ordered = 0;
 delete from userBasket where uID='user1231' and goodsName ='여름옷 시리즈~_2201140900' and addDate = '2022-01-17 18:15:39';
 insert into userBasket value('user1231',  '2022-01-17 18:15:39',  '여름옷 시리즈~_2201140900',  0, 0,  0,  1,  1,  80000, 0);
+
 -- --////////////// 업데이트 //////////////--  -- 
 set SQL_SAFE_UPDATES = 0;
 
